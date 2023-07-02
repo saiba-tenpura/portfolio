cdec8d0578e17cfb3d31a3513d558e1bbf42cbcc
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-02 15:37:37 +0200

    Setup navbar to allow for toggling navbar on mobile.

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,10 +1,14 @@
 <template>
-  <nav class="w-full p-2">
+  <nav class="w-full py-2 px-4 md:px-2">
     <div class="max-w-4xl flex flex-wrap justify-between items-center mx-auto">
       <div>
         Logo
       </div>
-      <div class="justify-between items-center w-full md:flex md:w-auto">
+      <button @click="(show = !show)" class="inline-flex items-center p-2 rounded-lg md:hidden">
+        <span class="sr-only">Toggle navigation menu</span>
+        <font-awesome-icon :icon="show ? 'fa-xmark' : 'fa-bars'" />
+      </button>
+      <div class="justify-between items-center w-full md:flex md:w-auto" :class="{ hidden: ! show }" :aria-expanded="show">
         <ul class="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
           <li>
             About
@@ -17,9 +21,10 @@
           </li>
         </ul>
       </div>
-      <div>
-        Placeholder
-      </div>
     </div>
   </nav>
 </template>
+
+<script lang="ts" setup>
+  const show = ref(false)
+</script>
