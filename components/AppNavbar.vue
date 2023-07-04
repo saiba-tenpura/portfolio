2b92f3939d6034a29b50369f7b0f8e4919bb7d3c
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-04 21:45:30 +0200

    Addv Navbar navigation links to pages.

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,23 +1,19 @@
 <template>
   <nav class="w-full p-2 sm:px-2">
     <div class="max-w-3xl flex flex-wrap justify-between items-center mx-auto">
-      <div>
+      <NuxtLink to="/">
         Logo
-      </div>
+      </NuxtLink>
       <button @click="(show = !show)" class="inline-flex items-center p-2 rounded-lg md:hidden">
         <span class="sr-only">Toggle navigation menu</span>
         <font-awesome-icon :icon="show ? 'fa-xmark' : 'fa-bars'" />
       </button>
       <div class="justify-between items-center w-full md:flex md:w-auto" :class="{ hidden: ! show }" :aria-expanded="show">
         <ul class="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-          <li>
-            About
-          </li>
-          <li>
-            Articles
-          </li>
-          <li>
-            Contact
+          <li v-for="link of navigation">
+            <NuxtLink :to="link.path">
+              {{ link.title }}
+            </NuxtLink>
           </li>
         </ul>
       </div>
@@ -27,4 +23,19 @@
 
 <script lang="ts" setup>
   const show = ref(false)
+
+  const navigation = [
+    {
+      "title": "About",
+      "path": "/",
+    },
+    {
+      "title": "Articles",
+      "path": "/articles",
+    },
+    {
+      "title": "Contact",
+      "path": "/contact",
+    }
+  ]
 </script>
