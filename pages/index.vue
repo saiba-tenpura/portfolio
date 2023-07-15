b385d9f370207ccb6986e2de4c7b8bf76a7e1cfb
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-15 13:46:59 +0200

    Add basic typography styling for index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex items-center justify-center h-100">
     <div class="text-center">
-      <h1>Saiba Tenpura</h1>
-      <h2>Full-Stack Developer / DevOps / Game Dev Hobbyist</h2>
-      <NuxtLink to="https://github.com/saiba-tenpura" class="icon-link-hover" target="_blank">
-        <font-awesome-icon icon="fa-brands fa-github" />
-      </NuxtLink>
+      <h1 class="text-3xl font-bold py-2">Saiba Tenpura</h1>
+      <h2 class="text-lg">Full-Stack Developer / DevOps / Hobbyist Game Dev</h2>
+      <div class="py-2">
+        <NuxtLink to="https://github.com/saiba-tenpura" class="icon-link-hover" target="_blank">
+          <font-awesome-icon icon="fa-brands fa-github" size="xl" />
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
