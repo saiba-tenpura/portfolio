3ec5064cbf20f54a841c5c8d6c55d16106593ff4
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-18 22:23:45 +0200

    Add work related text for about section.

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h2>About</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio esse labore soluta cum excepturi, obcaecati doloribus culpa ad possimus reiciendis beatae magnam quod quas repellat eos ex? Minus, ipsum cum.</p>
+    <h1 class="text-3xl font-bold py-2">About</h1>
+    <p>
+      Hey I'm Jan, a 25 year old passionate <span class="font-bold">Full Stack Developer</span> based in Germany.
+      My expertise includes the creation of web applications taking them from the design phase all the way to implementation as well as contionusly improving and maintaining them.
+    </p>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center justify-center h-100">
     <div class="text-center">
       <h1 class="text-3xl font-bold py-2">Saiba Tenpura</h1>
-      <h2 class="text-lg">Full-Stack Developer / DevOps / Hobbyist Game Dev</h2>
+      <h2 class="text-lg">Full Stack Developer / DevOps / Hobbyist Game Dev</h2>
       <div class="py-2">
         <NuxtLink to="https://github.com/saiba-tenpura" class="link-hover" target="_blank">
           <font-awesome-icon icon="fa-brands fa-github" size="xl" />
