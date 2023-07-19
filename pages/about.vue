a8f116eb56ce49d3c18943abc78dee386c12c130
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-19 22:39:45 +0200

    Add additional details to about page.

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,54 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold py-2">About</h1>
-    <p>
-      Hey I'm Jan, a 25 year old passionate <span class="font-bold">Full Stack Developer</span> based in Germany.
-      My expertise includes the creation of web applications taking them from the design phase all the way to implementation as well as contionusly improving and maintaining them.
-    </p>
+    <div class="mb-4">
+      <h1 class="text-3xl font-bold py-2">About</h1>
+      <p class="mb-4">
+        Hey I'm Jan, a 25 year old passionate <span class="font-bold">Full Stack Developer</span> based in Germany.
+        My expertise includes the creation of web applications taking them from the design phase all the way to implementation as well as contionusly improving and maintaining them.
+      </p>
+      <p>
+        I spent most of my spare time studying new technologies, ricing my linux desktop, playing around with game engines as well as 3D modeling and 3D animation.
+      </p>
+    </div>
+    <div>
+      <h2 class="text-2xl font-bold py-2">Skills</h2>
+      <div class="mb-4">
+        <h3>Frontend</h3>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>Git</li>
+          <li>Bootstrap</li>
+          <li>Tailwind CSS</li>
+          <li>SCSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Next.js</li>
+          <li>Vue.js</li>
+          <li>Nuxt.js</li>
+        </ul>
+      </div>
+      <div class="mb-4">
+        <h3>Backend</h3>
+        <ul>
+          <li>PHP</li>
+          <li>Laravel</li>
+          <li>Linux</li>
+          <li>MySQL</li>
+          <li>Redis</li>
+        </ul>
+      </div>
+      <div class="mb-4">
+        <h3>DevOps</h3>
+        <ul>
+          <li>Ansible</li>
+          <li>Bash</li>
+          <li>Docker</li>
+          <li>Linux</li> 
+          <li>Jenkins</li>
+          <li>REST APIs</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
