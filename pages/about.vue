837062716952ad5594a24b98890bb01a9414a3e0
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-20 22:13:15 +0200

    Generate skill list, slightly optimize about text and add basic styling.

--- pages/about.vue
+++ pages/about.vue
@@ -1,53 +1,56 @@
+<script setup lang="ts">
+  const skill_list= {
+    "Frontend": [
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "Tailwind CSS",
+      "Nuxt.js",
+      "Vue.js",
+      "Next.js",
+      "Bootstrap",
+    ],
+    "Backend": [
+      "PHP",
+      "Laravel",
+      "MySQL",
+      "REST APIs",
+      "Redis",
+    ],
+    "DevOps": [
+      "Bash",
+      "Git",
+      "Linux",
+      "Ansible",
+      "Docker",
+      "Jenkins",
+    ],
+  }
+</script>
+
 <template>
   <div>
     <div class="mb-4">
       <h1 class="text-3xl font-bold py-2">About</h1>
-      <p class="mb-4">
-        Hey I'm Jan, a 25 year old passionate <span class="font-bold">Full Stack Developer</span> based in Germany.
-        My expertise includes the creation of web applications taking them from the design phase all the way to implementation as well as contionusly improving and maintaining them.
-      </p>
-      <p>
-        I spent most of my spare time studying new technologies, ricing my linux desktop, playing around with game engines as well as 3D modeling and 3D animation.
-      </p>
+      <div class="text-gray-700 dark:text-gray-400">
+        <p class="mb-4">
+          Hey I'm Jan, a 25 year old passionate <span class="font-bold">Full Stack Developer</span> based in Germany.
+          My expertise lies in creating web applications, guiding them from the design phase to the full implementation, and ensuring their contionus improvement and maintenance.
+        </p>
+        <p>
+          In my spare time, I like to learn about new technologies, tweaking my Linux desktop, and experiment with game engines, 3D modeling as well as 3D animation.
+        </p>
+      </div>
     </div>
     <div>
       <h2 class="text-2xl font-bold py-2">Skills</h2>
-      <div class="mb-4">
-        <h3>Frontend</h3>
-        <ul>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>Git</li>
-          <li>Bootstrap</li>
-          <li>Tailwind CSS</li>
-          <li>SCSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Next.js</li>
-          <li>Vue.js</li>
-          <li>Nuxt.js</li>
-        </ul>
-      </div>
-      <div class="mb-4">
-        <h3>Backend</h3>
-        <ul>
-          <li>PHP</li>
-          <li>Laravel</li>
-          <li>Linux</li>
-          <li>MySQL</li>
-          <li>Redis</li>
-        </ul>
-      </div>
-      <div class="mb-4">
-        <h3>DevOps</h3>
-        <ul>
-          <li>Ansible</li>
-          <li>Bash</li>
-          <li>Docker</li>
-          <li>Linux</li> 
-          <li>Jenkins</li>
-          <li>REST APIs</li>
-        </ul>
+      <div class="flex justify-between">
+        <div v-for="(skills, category) in skill_list" class="mb-4">
+          <h3 class="text-lg font-bold py-2">{{ category }}</h3>
+          <ul class="list-disc list-inside text-gray-700 dark:text-gray-400">
+            <li v-for="skill in skills">{{ skill }}</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
