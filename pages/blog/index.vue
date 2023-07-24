d5e5fbea165c7a13d7990c73a89d065fb44d92b7
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-24 20:47:25 +0200

    Add empty state for blog entries.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,22 @@
+<script setup lang="ts">
+  const { data } = await useAsyncData(() => {
+    return queryContent('/blog/')
+      .sort({ created_at: -1 })
+      .find()
+  })
+</script>
+
 <template>
-  <div>
+  <section v-if="Array.isArray(data) && data.length > 0">
     <h1 class="text-3xl font-bold py-2 mb-4">Blog</h1>
-    <BlogEntries />
-  </div>
+    <BlogEntry v-for="blog_entry in data" :key="blog_entry._id" :blog_entry="blog_entry" />
+  </section>
+  <section v-else class="flex items-center justify-center">
+    <div class="text-center">
+      <h2 class="text-2xl font-bold mb-3">There are no blog entries yet!</h2>
+      <div class="text-zinc-700 dark:text-zinc-300">
+        Please check back later when some have been added.
+      </div>
+    </div>
+  </section>
 </template>
