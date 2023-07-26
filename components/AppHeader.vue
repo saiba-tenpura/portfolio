b8522263f9852e958c72208aa9c9b58c9b88fe38
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-26 22:53:48 +0200

    Add dynamic box shadow to header on scroll.

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,23 @@
+<script setup>
+  onMounted(() => {
+    const header = document.querySelector('#header')
+    const observer = new IntersectionObserver(
+      ([entry]) => entry.target.classList.toggle('stuck', entry.intersectionRatio < 1),
+      { threshold: [1] }
+    )
+
+    observer.observe(header)
+  })
+</script>
+
 <template>
-  <header class="sticky top-0 bg-zinc-50 dark:bg-zinc-900">
+  <header id="header" class="sticky top-[-1px] bg-zinc-50 dark:bg-zinc-900">
     <AppNavbar />
   </header>
 </template>
+
+<style scoped>
+#header.stuck {
+  @apply shadow-lg;
+}
+</style>
