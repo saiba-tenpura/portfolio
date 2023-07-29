fa022f93fe4711de10200a4024348fdf9441d433
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-29 22:38:41 +0200

    Adjust article prose colors and dark mode.

--- layouts/article.vue
+++ layouts/article.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="max-w-4xl py-4 px-6 mx-auto">
-    <article class="prose">
+    <article class="prose prose-zinc dark:prose-invert">
       <ContentDoc />
     </article>
   </main>
