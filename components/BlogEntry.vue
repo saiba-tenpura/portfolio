1b6075d54d2f55252e378ede57b41865d5923bf5
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-07-30 22:38:49 +0200

    Adjust blog list preview image rendering.

--- components/BlogEntry.vue
+++ components/BlogEntry.vue
@@ -22,12 +22,12 @@ defineProps({
 <template>
   <article>
     <NuxtLink :to="blog_entry._path" class="flex flex-col md:flex-row">
-      <div class="object-cover w-full rounded-lg h-48 md:h-auto md:w-48">
-        <NuxtImg
-          :src="blog_entry.cover"
-          :alt="blog_entry.title"
-        />
-      </div>
+      <NuxtImg
+        :src="blog_entry.cover"
+        :alt="blog_entry.title"
+        sizes="sm:100vw md:100vw lg:100vw"
+        class="object-cover w-full rounded-lg h-48 md:h-auto md:w-48"
+      />
       <div class="flex flex-col justify-between p-4">
         <h2 class="text-lg font-bold mb-2 tracking-tight text-zinc-900 dark:text-white">{{ blog_entry.title }}</h2>
         <p class="mb-3 text-zinc-700 dark:text-zinc-300">{{ blog_entry.description }}</p>
