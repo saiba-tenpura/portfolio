b8bcd3696580f2e3ba19e0b68620cea2e2f37147
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-08-12 21:50:20 +0200

    Add primary button for link to dotfiles.

---
+++ components/content/CustomButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  defineProps({
+    type: {
+      type: String,
+    }
+  })
+  const types = {
+    primary: "text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800"
+  }
+</script>
+
+<template>
+  <button
+    type="button"
+    class="font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    :class="types[type]"
+    >
+    <ContentSlot :use="$slots.default" unwrap="p" />
+  </button>
+</template>
