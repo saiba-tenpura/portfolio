a4b9a1142a9cc5988a2a7be13f0b70643d104b6c
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-10-25 22:00:45 +0200

    Parameterize label for table of contents.

--- components/TableOfContents.vue
+++ components/TableOfContents.vue
@@ -2,9 +2,10 @@
 import type { TocLink } from '@nuxt/content/dist/runtime/types';
 
 withDefaults(defineProps<{
- links: TocLink[];
- sticky?: boolean;
- current?: string;
+  ariaLabelledby?: string;
+  links: TocLink[];
+  sticky?: boolean;
+  current?: string;
 }>(), {
   sticky: false,
 });
@@ -14,7 +15,7 @@ withDefaults(defineProps<{
   <aside>
     <div :class="{ 'sticky top-32': sticky }">
       <h2 class="text-lg font-bold mb-2 tracking-tight text-zinc-900 dark:text-white" :class="{ 'mt-10': sticky }">Table of Contents</h2>
-      <nav aria-labelledby="article-navigation">
+      <nav :aria-labelledby="ariaLabelledby">
         <TocSection :links="links" :current="current" />
       </nav>
     </div>
