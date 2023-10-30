e7de926f1e9381df33bb9fff4a7d826977c43366
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-10-30 20:11:34 +0100

    Add a custom error page.

---
+++ error.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+  error: {
+    url: string,
+    statusCode: number,
+    statusMessage: string,
+    message: string,
+    description: string,
+    data: any,
+  },
+}>();
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
+
+<template>
+  <main class="flex min-h-screen">
+    <section class="flex flex-auto items-center justify-center">
+      <div class="text-center">
+        <h1 class="mb-4 text-7xl lg:text-9xl">{{ error.statusCode }}</h1>
+        <p class="mb-4 text-lg">{{ error.message }}</p>
+        <CustomButton class="px-5 py-2.5" type="primary" position="center" @click="handleError">
+          Back to home
+        </CustomButton>
+      </div>
+    </section>
+  </main>
+</template>
