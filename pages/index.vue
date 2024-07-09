e5940cf5b0be0d1c89aa0b30acc5406588bf1bd8
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-07-09 21:08:02 +0200

    Add simple animation for displaying roles to the landing page.

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,17 @@ useSeoMeta({
   <section class="flex items-center justify-center">
     <div class="text-center">
       <h1 class="text-3xl font-bold py-2">Saiba Tenpura</h1>
-      <h2 class="text-lg">Full Stack Developer / DevOps / Hobbyist Game Dev</h2>
+      <section id="title-animation" class="h-7 text-lg overflow-hidden">
+        <div>
+          <div class="inline-block">Full Stack Developer</div>
+        </div>
+        <div>
+          <div class="inline-block">DevOps</div>
+        </div>
+        <div>
+          <div class="inline-block">Hobbyist Game Dev</div>
+        </div>
+      </section>
       <div class="py-2">
         <NuxtLink v-for="(link, social) in socials" :to="link" class="link-hover" target="_blank">
           <font-awesome-icon :icon="'fa-brands fa-' + social" size="xl" />
@@ -20,3 +30,42 @@ useSeoMeta({
     </div>
   </section>
 </template>
+
+<style scoped>
+#title-animation div:first-child {
+  animation: text-swap 8s infinite;
+}
+
+@keyframes text-swap {
+  0% {
+    margin-top: 0;
+  }
+  10% {
+    margin-top: 0;
+  }
+  20% {
+    margin-top: -1.75rem;
+  }
+  30% {
+    margin-top: -1.75rem;
+  }
+  40% {
+    margin-top: -3.50rem;
+  }
+  60% {
+    margin-top: -3.50rem;
+  }
+  70% {
+    margin-top: -1.75rem;
+  }
+  80% {
+    margin-top: -1.75rem;
+  }
+  90% {
+    margin-top: 0;
+  }
+  100% {
+    margin-top: 0;
+  }
+}
+</style>
