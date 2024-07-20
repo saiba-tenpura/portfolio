7a3f62aff1a0d1216b2938fd964b2be2f5a70e1f
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-07-20 17:12:55 +0200

    Fix syntax for loading bar component.

--- components/AppLoadingBar.vue
+++ components/AppLoadingBar.vue
@@ -1,88 +1,90 @@
 <script setup lang="ts">
-  import { useNuxtApp, onBeforeUnmount, reactive } from '#imports'
+import { useNuxtApp, onBeforeUnmount, reactive } from '#imports';
 
-  const props = defineProps({
-    throttle: {
-      type: Number,
-      default: 200
-    },
-    duration: {
-      type: Number,
-      default: 2000
-    },
-    height: {
-      type: Number,
-      default: 3
-    }
-  })
+const props = defineProps({
+  throttle: {
+    type: Number,
+    default: 200,
+  },
+  duration: {
+    type: Number,
+    default: 2000,
+  },
+  height: {
+    type: Number,
+    default: 3,
+  },
+});
 
-  const nuxtApp = useNuxtApp()
+const nuxtApp = useNuxtApp();
 
-  // Options & Data
-  const data = reactive({
-    percent: 0,
-    show: false,
-    canSucceed: true
-  })
+// Options & Data
+const data = reactive({
+  percent: 0,
+  show: false,
+  canSucceed: true,
+});
 
-  // Local variables
-  let _timer = null
-  let _throttle = null
-  let _cut
+// Local variables
+let _timer = null;
+let _throttle = null;
+let _cut;
 
-  // Functions
-  function clear () {
-    _timer && clearInterval(_timer)
-    _throttle && clearTimeout(_throttle)
-    _timer = null
+// Functions
+function clear () {
+  _timer && clearInterval(_timer);
+  _throttle && clearTimeout(_throttle);
+  _timer = null;
+}
+
+function start () {
+  if (data.show) {
+    return;
   }
 
-  function start () {
-    if (data.show) { return }
-
-    clear()
-    data.percent = 0
-    data.canSucceed = true
-    if (props.throttle) {
-      _throttle = setTimeout(startTimer, props.throttle)
-    } else {
-      startTimer()
-    }
+  clear();
+  data.percent = 0;
+  data.canSucceed = true;
+  if (props.throttle) {
+    _throttle = setTimeout(startTimer, props.throttle);
+  } else {
+    startTimer();
   }
+}
 
-  function increase (num) {
-    data.percent = Math.min(100, Math.floor(data.percent + num))
-  }
+function increase (num) {
+  data.percent = Math.min(100, Math.floor(data.percent + num));
+}
 
-  function finish () {
-    data.percent = 100
-    hide()
-  }
+function finish () {
+  data.percent = 100;
+  hide();
+}
 
-  function hide () {
-    clear()
+function hide () {
+  clear();
+  setTimeout(() => {
+    data.show = false;
     setTimeout(() => {
-      data.show = false
-      setTimeout(() => {
-        data.percent = 0
-      }, 400)
-    }, 500)
-  }
+      data.percent = 0;
+    }, 400);
+  }, 500);
+}
 
-  function startTimer () {
-    data.show = true
-    _cut = 10000 / Math.floor(props.duration)
-    _timer = setInterval(() => {
-      increase(_cut)
-    }, 100)
-  }
+function startTimer () {
+  data.show = true;
+  _cut = 10000 / Math.floor(props.duration);
+  _timer = setInterval(() => {
+    increase(_cut);
+  }, 100);
+}
 
-  // Hooks
-  nuxtApp.hook('content:middleware:start', start)
-  nuxtApp.hook('page:start', start)
-  nuxtApp.hook('page:finish', finish)
+// Hooks
+nuxtApp.hook('content:middleware:start', start);
+nuxtApp.hook('page:start', start);
+nuxtApp.hook('page:finish', finish);
 
-  onBeforeUnmount(() => clear)
+onBeforeUnmount(() => clear);
 </script>
 
 <template>
@@ -103,15 +105,15 @@
 </template>
 
 <style scoped>
-  .nuxt-progress {
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    right: 0px;
-    width: 0%;
-    opacity: 1;
-    transition: width 0.1s, height 0.4s, opacity 0.4s;
-    background: repeating-linear-gradient(to right, #00dc82 0%, #34cdfe 50%, #0047e1 100%);
-    z-index: 999999;
-  }
+.nuxt-progress {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  width: 0%;
+  opacity: 1;
+  transition: width 0.1s, height 0.4s, opacity 0.4s;
+  background: repeating-linear-gradient(to right, #00dc82 0%, #34cdfe 50%, #0047e1 100%);
+  z-index: 999999;
+}
 </style>
