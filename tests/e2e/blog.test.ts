8a168f655c10c9b79df343ff0b0f84de2a03527d
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-08-19 09:26:57 +0200

    Add unit test for verifying content of existing blog entry.

--- tests/e2e/blog.test.ts
+++ tests/e2e/blog.test.ts
@@ -4,6 +4,11 @@ import { describe, expect, test } from 'vitest';
 describe('blog', async () => {
   await setup();
 
+  test('existing page', async () => {
+    const response = await fetch('/blog/my-portfolio').then(r => r.text());
+    expect(response).toContain('My Portfolio');
+  });
+
   test('404 error page', async () => {
     const response = await fetch('/blog/non-existent').then(r => r.text());
     expect(response).toContain('Blog entry not found!');
