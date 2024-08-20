6c4e25540b5a1b1845c3913be06f1ea16a694a6f
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-08-20 10:26:06 +0200

    Utitilize playwright for blog table of contents testing.

--- tests/e2e/blog.test.ts
+++ tests/e2e/blog.test.ts
@@ -1,12 +1,14 @@
-import { fetch, setup } from '@nuxt/test-utils/e2e';
+import { fetch, setup, createPage } from '@nuxt/test-utils/e2e';
 import { describe, expect, test } from 'vitest';
 
 describe('blog', async () => {
   await setup();
 
-  test('existing page', async () => {
-    const response = await fetch('/blog/my-portfolio').then(r => r.text());
-    expect(response).toContain('My Portfolio');
+  test('existing entry & table of contents', async () => {
+    const page = await createPage('/blog/my-portfolio');
+    expect(await page.getByRole('heading', { name: 'My Portfolio' }).isVisible()).toBe(true);
+    expect(await page.getByRole('link', { name: 'Tech Stack' }).getAttribute('href')).toBe('#tech-stack');
+    expect(await page.getByRole('link', { name: 'Logo' }).last().getAttribute('href')).toBe('#logo');
   });
 
   test('404 error page', async () => {
