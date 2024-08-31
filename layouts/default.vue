c491924780f80f0dbe6c0bdad661d6dfbeef9ec2
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-08-31 12:43:04 +0200

    Adjust hero section layout further.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-auto max-w-5xl py-4 px-6 mx-auto">
+  <main class="flex flex-auto max-w-6xl py-4 px-6 mx-auto">
     <slot />
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -9,38 +9,17 @@ useSeoMeta({
 </script>
 
 <template>
-  <section class="grid lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-    <div class="lg:col-span-5 lg:order-last mx-auto">
-      <NuxtImg
-        src="/logo-render.png"
-        alt="logo-render"
-        sizes="sm:75vw md:35vw lg:25vw"
-        loading="lazy"
-      />
-    </div>
-    <div class="lg:col-span-7">
-      <h1 class="mb-4 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl">Saiba Tenpura</h1>
-      <p class="mb-6 font-light text-neutral-500 lg:mb-8 md:text-lg lg:text-xl dark:text-neutral-400">
-        {{ roles.join(' / ') }}
-      </p>
-      <CustomButton variant="primary" as="a" href="https://github.com/saiba-tenpura/dotfiles">
-        <font-awesome-icon class="mr-2" icon="fa-brands fa-github" size="xl" />
-        Checkout my GitHub!
-      </CustomButton>
-    </div>
-  </section>
-  <!--
-  <section class="flex flex-col md:flex-row-reverse items-center justify-center">
+  <section class="flex flex-col lg:flex-row-reverse items-center justify-center">
     <div>
       <NuxtImg
         src="/logo-render.png"
         alt="logo-render"
-        sizes="sm:75vw md:35vw lg:25vw"
+        sizes="sm:75vw md:55vw lg:25vw"
         loading="lazy"
       />
     </div>
-    <div>
-      <h1 class="mb-4 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl">Saiba Tenpura</h1>
+    <div class="text-center lg:text-left lg:mr-16">
+      <h1 class="mb-2 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl">Saiba Tenpura</h1>
       <p class="mb-6 font-light text-neutral-500 lg:mb-8 md:text-lg lg:text-xl dark:text-neutral-400">
         {{ roles.join(' / ') }}
       </p>
@@ -50,5 +29,4 @@ useSeoMeta({
       </CustomButton>
     </div>
   </section>
-  -->
 </template>
