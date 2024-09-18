7472c98ce4ace2d6cc680acc4c22a81c1388f6c0
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-09-18 20:42:56 +0200

    Add new custom color scheme to tailwind.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss';
-import colors from 'tailwindcss/colors';
 
 export default <Partial<Config>>{
   content: [
@@ -14,8 +13,32 @@ export default <Partial<Config>>{
   theme: {
     extend: {
       colors: {
-        primary: colors.sky,
-        secondary: colors.fuchsia,
+        primary: {
+          50:  '#e6f1fe',
+          100: '#cee3fd',
+          200: '#9cc7fc',
+          300: '#6babfa',
+          400: '#398ff9',
+          500: '#0874f7',
+          600: '#065cc6',
+          700: '#054594',
+          800: '#032e63',
+          900: '#021731',
+          950: '#010c19',
+        },
+        secondary: {
+          50:  '#fbe7fe',
+          100: '#f7cefd',
+          200: '#ee9efa',
+          300: '#e66df8',
+          400: '#dd3cf6',
+          500: '#d50bf4',
+          600: '#aa09c3',
+          700: '#800792',
+          800: '#550561',
+          900: '#2b0231',
+          950: '#150118',
+        },
         accent: {
           50:  '#f3f2f3',
           100: '#dad8da',
