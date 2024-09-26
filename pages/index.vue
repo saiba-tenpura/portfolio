2a2f747ae7f18f9660aa8f94ea6666b3e64c1bfe
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-09-26 22:23:04 +0200

    Adjust landing page layout to use grid.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="flex flex-col lg:flex-row-reverse items-center justify-center">
-    <div>
+  <section class="grid gap-4 m-auto px-2 md:px-8 lg:grid-cols-12 lg:gap-0 lg:px-0">
+    <div class="mx-auto lg:col-span-5">
       <NuxtImg
         src="/logo-render.png"
         alt="logo-render"
@@ -8,9 +8,9 @@
         loading="lazy"
       />
     </div>
-    <div class="text-center lg:text-left lg:mr-16">
-      <h1 class="mb-2 text-4xl font-extrabold tracking-tight md:text-5xl xl:text-6xl">Full Stack Developer / Hobbyist Game Dev</h1>
-      <p class="mb-6 font-light text-neutral-500 lg:mb-8 md:text-lg lg:text-xl dark:text-neutral-400">
+    <div class="place-self-center lg:col-span-7 lg:-order-1">
+      <h1 class="text-3xl font-extrabold tracking-tight mb-3 md:text-4xl xl:text-6xl">Full Stack Developer</h1>
+      <p class="font-light text-neutral-500 mb-6 lg:mb-8 md:text-lg lg:text-xl dark:text-neutral-400">
         Hi, I'm Jan, a dedicated full stack developer with a passion for problem solving, technology, and learning. Taking web applications from concept to completion, I enjoy experimenting with new technologies and exploring the intersection of art and science through 3D modeling and game development.
       </p>
       <CustomButton class="mx-auto" variant="primary" as="a" href="https://github.com/saiba-tenpura/dotfiles">
