f48922d2f60fbdf053bba418ea56d74e5692e0e3
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2025-08-09 15:30:57 +0200

    Add custom icons for 404 and 500 errors.

--- error.vue
+++ error.vue
@@ -12,7 +12,11 @@ const handleError = () => clearError({ redirect: '/' });
   <main class="flex min-h-screen">
     <section class="flex flex-auto items-center justify-center">
       <div class="text-center">
-        <h1 class="mb-3 text-7xl lg:text-9xl">{{ error?.statusCode }}</h1>
+        <div class="mb-3">
+          <NuxtImg v-if="error?.statusCode == 404" src="/404.svg" />
+          <NuxtImg v-else="error?.statusCode == 500" src="/500.svg" />
+          <h1 v-else class="text-7xl lg:text-9xl">{{ error?.statusCode }}</h1>
+        </div>
         <p class="mb-5 text-lg">{{ error?.message }}</p>
         <CustomButton class="px-5 py-2.5" variant="primary" @click="handleError">
           Back to home
