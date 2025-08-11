e84c5dd1154bfbe58246772a3c0ed0125f3ca6d1
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2025-08-11 11:01:47 +0200

    Adjust error message for 404 page.

--- error.vue
+++ error.vue
@@ -17,9 +17,12 @@ const handleError = () => clearError({ redirect: '/' });
           <NuxtImg v-else="error?.statusCode == 500" src="/500.svg" />
           <h1 v-else class="text-7xl lg:text-9xl">{{ error?.statusCode }}</h1>
         </div>
-        <p class="mb-5 text-lg">{{ error?.message }}</p>
+        <div class="text-lg mb-5">
+          <p v-if="error?.statusCode != 404">{{ error?.message }}</p>
+          <p v-else>The page you're looking for doesn't exist.</p>
+        </div>
         <CustomButton class="px-5 py-2.5" variant="primary" @click="handleError">
-          Back to home
+          Return to the homepage
         </CustomButton>
       </section>
     </NuxtLayout>
